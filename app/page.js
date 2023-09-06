222a3da59d6eeadd59dcfabbf3fe352a1de62679
insane-git <b03036454455@gmail.com>
2023-09-07 01:11:19 +0300

    thirf commit

--- app/page.js
+++ app/page.js
@@ -1,31 +1,21 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
-
+import { FaCaretDown, FaUserAlt } from "react-icons/fa";
 const GptForm = () => {
-  const [messages, setMessages] = useState([]);
-  const [prompt, setPrompt] = useState("");
-  const [response, setResponse] = useState("");
-  const [serverMessagesArray, setServerMessagesArray] = useState([]);
-  const messagesContainerRef = useRef(null);
-  const [responseTest, setResponseTest] = useState("");
-  const [abortController, setAbortController] = useState(null);
+  const [message, setMessage] = useState({
+    prompt: "",
+    response: "",
+  });
+  const textareaRef = useRef(null);
   const [scrollStatus, setScrollStatus] = useState(false);
   const [initialMessages, setInitialMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [copyStatus, setCopyStatus] = useState([]);
+  const containerRef = useRef(null);
+  const codeBlocks = useRef([]);
+  const [abortController, setAbortController] = useState(null);
 
-  useEffect(() => {
-    const retrieveMessages = async () => {
-      const storedMessages = await JSON.parse(
-        localStorage.getItem("messages") || "[]"
-      );
-      setMessages(storedMessages);
-      setInitialMessages(storedMessages);
-    };
-    retrieveMessages();
-  }, []);
-
-  useEffect(() => {
-    window.scrollTo(0, document.body.scrollHeight);
-  }, [initialMessages]);
+  const [submitStatus, setSubmitStatus] = useState(false);
   useEffect(() => {
     if (document && typeof document !== "undefined") {
       document.addEventListener("scroll", () => {
@@ -45,19 +35,32 @@ const GptForm = () => {
       window.scrollTo(0, document.body.scrollHeight);
     }
   });
+  useEffect(() => {
+    const retrieveMessages = async () => {
+      const storedMessages = await JSON.parse(
+        localStorage.getItem("messages") || "[]"
+      );
+      setMessages(storedMessages);
+      setInitialMessages(storedMessages);
+    };
+    retrieveMessages();
+  }, []);
+  useEffect(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, [initialMessages]);
 
-  const handleCancel = () => {
-    if (abortController) {
-      abortController.abort();
-    }
-    setAbortController(null);
-  };
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
+      setSubmitStatus(true);
       setMessages((prevMessages) => [
         ...(prevMessages || []),
-        { prompt, response: "" },
+        { role: "user", content: message.prompt },
+        {
+          role: "assistant",
+          content: "",
+        },
       ]);
       const controller = new AbortController();
       setAbortController(controller);
@@ -68,12 +71,11 @@ const GptForm = () => {
         },
         signal: controller.signal,
         body: JSON.stringify({
-          prompt,
-          messagesArray: serverMessagesArray,
-          
+          prompt: message.prompt,
+          messagesArray: messages,
         }),
       });
-      setPrompt("");
+      setMessage({ prompt: "" });
       const reader = res.body.getReader();
       const decoder = new TextDecoder();
       let done = false;
@@ -84,36 +86,38 @@ const GptForm = () => {
         done = doneReading;
         const chunk = decoder.decode(value);
         responseData += chunk;
-        setResponseTest(responseData);
-        setMessages((prevMessages) => [
-          ...prevMessages.slice(0, -1),
-          {
-            prompt,
-            response: prevMessages[prevMessages.length - 1].response + chunk,
-          },
-        ]);
-      }
-      setResponse(responseData);
-      setServerMessagesArray((prevMessages) => [
-        ...prevMessages,
 
-        { role: "user", content: prompt },
-        { role: "assistant", content: response },
-      ]);
-      const updatedMessages = [...messages, { prompt, response: responseData }];
-      localStorage.setItem("messages", JSON.stringify(updatedMessages));
-      setAbortController(null);
-    } catch (error) {
-      if (error.name === "AbortError") {
-        // Request was cancelled
-        console.log("Request was cancelled");
-      } else {
-        // Handle other errors
-        console.error("Error:", error);
+        // setMessages((prevMessages) => [
+        //   ...prevMessages.slice(0, -1),
+        //   {
+        //     role: "assistant",
+        //     content: prevMessages[prevMessages.length - 1].content + chunk,
+        //   },
+        // ]);
+        setMessages((prevMessages) => {
+          const updatedMessages = [...prevMessages];
+          updatedMessages[updatedMessages.length - 1].content += chunk;
+          return updatedMessages;
+        });
       }
+      const updatedMessages = [
+        ...messages,
+        { role: "user", content: message.prompt },
+        { role: "assistant", content: responseData },
+      ];
+      localStorage.setItem("messages", JSON.stringify(updatedMessages));
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      // Clear the input field and focus on it after submission
+      setMessage({ ...message, prompt: "" });
+      if (textareaRef.current) {
+        textareaRef.current.focus();
+      }
+      setSubmitStatus(false);
+      setAbortController(null);
     }
   };
-
   useEffect(() => {
     return () => {
       if (abortController) {
@@ -122,77 +126,177 @@ const GptForm = () => {
       setAbortController(null);
     };
   }, []);
+  const handleCancel = () => {
+    if (abortController) {
+      abortController.abort();
 
-  const handleMessageChange = (e) => {
-    setPrompt(e.target.value);
+      localStorage.setItem("messages", JSON.stringify([...messages]));
+    }
+    setAbortController(null);
   };
-  const ClearChat = () => {
-    if (messages.length > 0 && window.confirm("Are you sure?")) {
+  const handleMessageChange = (e) => {
+    setMessage({ ...message, [e.target.name]: e.target.value });
+  };
+
+  const handleCopyClick = (index) => {
+    if (codeBlocks.current[index]) {
+      const codeBlockContent = codeBlocks.current[index].textContent;
+      const textArea = document.createElement("textarea");
+      textArea.value = codeBlockContent;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textArea);
+
+      // Update the copy status for this code block
+      const newCopyStatus = [...copyStatus];
+      newCopyStatus[index] = true;
+      setCopyStatus(newCopyStatus);
+
+      // Reset the copy status after a delay
+    }
+  };
+  useEffect(() => {
+    // Reset the copy status after 2 seconds
+    const timeout = setTimeout(() => {
+      const newCopyStatus = copyStatus.map(() => false);
+      setCopyStatus(newCopyStatus);
+    }, 2000);
+
+    // Clear the timeout when the component unmounts
+    return () => clearTimeout(timeout);
+  }, [copyStatus]);
+  const handleDelete = () => {
+    if (confirm("Are you sure?")) {
       setMessages([]);
       localStorage.removeItem("messages");
     }
   };
-
-
+  const scrollToBottom = () => {
+    containerRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  };
   return (
-   
-        <div
-          className="flex flex-col pt-10 max-w-[60vw] mx-auto min-h-screen"
-          ref={messagesContainerRef}>
-          <div className="flex flex-col justify-end flex-1 overflow-y-auto">
-            {messages?.map((message, index) => (
-              <div key={index}>
-                {message.prompt && (
-                  <div className="p-2 text-right rounded bg-slate-700">
-                    <p className="text-right">{message.prompt} : User</p>
-                  </div>
-                )}
-                {message.response && (
-                  <div className="p-2 text-left rounded bg-slate-800">
-                    <p>AI : {message.response} </p>
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
+    <div ref={containerRef}>
+      <div className="flex flex-col min-h-screen pt-10 mx-auto">
+        <div className="flex flex-col justify-end flex-1 mb-32 w-[80vw] mx-auto overflow-y-auto">
+          {messages?.map((message, index) => (
+            <div key={index}>
+              {message?.role === "user" && (
+                <div className="w-full p-2 rounded bg-slate-700">
+                  <p className="flex items-center">
+                    <FaUserAlt className="mx-1" /> : {message.content}
+                  </p>
+                </div>
+              )}
+              {message?.role === "assistant" && (
+                <div className="w-full p-2 rounded bg-slate-800">
+                  {message.content.split("```").map((part, index) => {
+                    if (index % 2 === 0) {
+                      return (
+                        <div className="w-[80%] px-2 py-4" key={index}>
+                          {part.split("\n").map((line, index) => (
+                            <p key={index}>{line}</p>
+                          ))}
+                        </div>
+                      );
+                    } else {
+                      const lines = part.split("\n");
+                      const codeContent = lines.slice(1).join("\n");
+                      return (
+                        <div
+                          className="relative px-4 py-4 text-white bg-black rounded-lg"
+                          key={index}>
+                          <pre ref={(el) => (codeBlocks.current[index] = el)}>
+                            {codeContent}
+                          </pre>
+                          <button
+                            className="absolute top-0 right-0 p-2 text-white rounded-sm bg-slate-900"
+                            onClick={() => handleCopyClick(index)}>
+                            {copyStatus[index] ? "Copied" : "Copy"}
+                          </button>
+                        </div>
+                      );
+                    }
+                  })}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
 
-          <div className="w-[60vw] min-h-20 flex justify-center items-center mx-auto">
-            <button onClick={ClearChat}>
-              <p className="text-sm text-center text-yellow-50">Clear chat</p>
-            </button>
+        <div className="fixed bottom-0 flex items-center justify-center w-full mx-auto bg-black bg-opacity-50 min-h-20 ">
+          <div
+            className="flex items-center justify-center p-4 max-md:flex-col"
+            style={{
+              margin: "0 auto",
+              width: "80%",
+            }}>
             <form
               onSubmit={handleSubmit}
-              className="flex justify-center w-full">
-              <textarea
-                value={prompt}
-                required
-                placeholder="Type your message here..."
-                className="w-full p-2 rounded bg-slate-900"
-                onChange={handleMessageChange}></textarea>
-              {abortController ? (
-                <button
-                  title="Note that this will cancel the request and the message will be lost"
-                  type="button"
-                  onClick={handleCancel}
-                  className="p-2 mx-1 text-white transition-all bg-red-600 rounded">
-                  Cancel
-                </button>
-              ) : (
-                <button
-                  disabled={!prompt}
-                  type="submit"
-                  className="p-2 mx-1 text-white transition-all rounded bg-slate-900 disabled:bg-slate-700">
-                  Submit
-                </button>
-              )}
+              className="flex items-center justify-center w-full">
+              <button
+                type="button"
+                className="p-1 transition-all rounded w-28 hover:bg-red-500"
+                onClick={handleDelete}>
+                Clear Chat
+              </button>
+              <div className="flex items-center justify-center w-full">
+                <textarea
+                  value={message.prompt}
+                  required
+                  ref={textareaRef}
+                  name="prompt"
+                  rows={!message.prompt && 1}
+                  placeholder="Type your message here..."
+                  className="w-full p-2 transition-all rounded resize-none bg-slate-900"
+                  style={{ maxHeight: "300px", height: "auto" }}
+                  onChange={handleMessageChange}
+                  onInput={(e) => {
+                    e.target.rows = 1;
+                    e.target.rows = Math.min(
+                      5,
+                      Math.ceil((e.target.scrollHeight - 16) / 20)
+                    );
+                  }}
+                />
+
+                {abortController ? (
+                  <button
+                    title="Note that this will cancel the request and the message will be lost"
+                    type="button"
+                    onClick={handleCancel}
+                    className="p-2 mx-1 text-white transition-all bg-red-600 rounded ">
+                    Cancel
+                  </button>
+                ) : (
+                  <button
+                    disabled={!message.prompt}
+                    type="submit"
+                    className="flex flex-col items-center justify-center h-8 p-2 mx-1 text-center transition-all rounded bg-slate-900 disabled:bg-slate-700">
+                    Submit
+                  </button>
+                )}
+              </div>
             </form>
           </div>
         </div>
-     
+      </div>
+      <div className="absolute">
+        <button
+          className={`fixed bottom-20 right-5 p-3 bg-slate-600  transition-all duration-300 z-50 rounded-full  ${
+            scrollStatus && "hidden"
+          }`}
+          onClick={scrollToBottom}
+          title="Scroll to bottom">
+          <FaCaretDown />
+        </button>
+      </div>
+    </div>
   );
 };
 
 export default GptForm;
-// export const dynamic = true;
-// export const fetchCache = "force-no-store";
-// export const revalidate = 0;
+export const dynamic = true;
